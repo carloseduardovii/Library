95a37f83ca6333e20740526e6e8bdc14ad86e6ce
Carlos Eduardo <carloseduardovii@gmail.com>
2022-08-11 17:08:41 -0500

    exercice 2.2

---
+++ books/permisions.py
@@ -0,0 +1,35 @@
+from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
+
+
+class IsLibraryUser(BasePermission):
+    """
+    Allows access only to admin users.
+    """
+
+    def has_permission(self, request, view):
+        if request.user.is_staff:
+            return True
+        else:
+            return bool(
+                request.method in SAFE_METHODS or
+                request.user and
+                request.user.is_authenticated
+            )
+
+class IsRent(BasePermission):
+    def has_object_permission(self,  request, view, obj):
+
+        if obj.is_rent:
+            if request.user.is_staff:
+                return True
+            elif request.user == obj.member:
+                return True
+            else:
+                return False
+        else:
+            if request.user.is_staff:
+                return True
+            else:
+                return False
+
+
--- books/views.py
+++ books/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Book, BookItem, User
-from .serializers import BookSerializer, BookItemSerializer, MemberSerializer
+from .serializers import BookSerializer, BookItemSerializer, BookItemCreateSerializer
+from .permisions import IsLibraryUser, IsRent
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -12,3 +15,17 @@ class BookViewSet(viewsets.ModelViewSet):
 class BookItemViewSet(viewsets.ModelViewSet):
     queryset = BookItem.objects.all()
     serializer_class = BookItemSerializer
+    permission_classes = [IsLibraryUser]
+
+    def get_serializer_class(self):
+        if self.action == "create" or self.action == "update":
+            return BookItemCreateSerializer
+        else:
+            return  BookItemSerializer
+
+    def get_permissions(self):
+        if self.action ==  "update":
+            permissions = [ IsRent]
+            return [permission() for permission in permissions]
+        else:
+            return [IsLibraryUser()]
